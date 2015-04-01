023d6a7e47891a00468736ee6cf33c1adfb2cc89
Shlomi Steinberg <p@shlomisteinberg.com>
2015-04-01 08:35:28 +0300

    Intel C++ compiler

---
+++ Simulation/main.cpp
@@ -0,0 +1,44 @@
+
+#include <Windows.h>
+#include <SFML/Graphics.hpp>
+#include <SFML/OpenGL.hpp>
+
+int CALLBACK WinMain(_In_  HINSTANCE hInstance,
+					 _In_  HINSTANCE hPrevInstance,
+					 _In_  LPSTR lpCmdLine,
+					 _In_  int nCmdShow) {
+	// create the window
+	sf::Window window(sf::VideoMode(800, 600), "Simulation - Shlomi Steinberg", sf::Style::Default, sf::ContextSettings(32));
+	window.setVerticalSyncEnabled(true);
+
+	// load resources, initialize the OpenGL states, ...
+
+	// run the main loop
+	bool running = true;
+	while (running) {
+		// handle events
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				// end the program
+				running = false;
+			}
+			else if (event.type == sf::Event::Resized) {
+				// adjust the viewport when the window is resized
+				glViewport(0, 0, event.size.width, event.size.height);
+			}
+		}
+
+		// clear the buffers
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// draw...
+
+		// end the current frame (internally swaps the front and back buffers)
+		window.display();
+	}
+
+	// release resources...
+
+	return 0;
+}
