fc0304702be0ccbc07431aa9d43b98fc1beb4e32
Shlomi Steinberg <p@shlomisteinberg.com>
2016-04-10 11:17:28 +0300

    TextureCubeMapArray

--- Simulation/src/ste/engine/core/gpu_resources/surfaces/textures/TextureCubeMapArray.hpp
+++ Simulation/src/ste/engine/core/gpu_resources/surfaces/textures/TextureCubeMapArray.hpp
@@ -12,26 +12,27 @@
 namespace StE {
 namespace Core {
 
-class TextureCubeMap : public texture_mipmapped<core_resource_type::TextureCubeMap> {
+class TextureCubeMapArray : public texture_mipmapped<core_resource_type::TextureCubeMapArray> {
 private:
-	using Base = texture_mipmapped<core_resource_type::TextureCubeMap>;
+	using Base = texture_mipmapped<core_resource_type::TextureCubeMapArray>;
 
 public:
-	TextureCubeMap(TextureCubeMap &&m) = default;
-	TextureCubeMap& operator=(TextureCubeMap &&m) = default;
+	TextureCubeMapArray(TextureCubeMapArray &&m) = default;
+	TextureCubeMapArray& operator=(TextureCubeMapArray &&m) = default;
 
-	TextureCubeMap(gli::format format, const typename Base::size_type &size, int levels = 1) : texture_mipmapped(format, size, levels) {}
-	TextureCubeMap(const gli::texture_cube &t, bool generate_mipmaps = false) :
+	TextureCubeMapArray(gli::format format, const typename Base::size_type &size, int levels = 1) : texture_mipmapped(format, size, levels) {}
+	TextureCubeMapArray(const gli::texture_cube_array &t, bool generate_mipmaps = false) :
 						texture_mipmapped(t.format(),
-										  t.extent(),
-										  generate_mipmaps ? calculate_mipmap_max_level(t[0].extent()) + 1 : t.levels(),
+										  typename Base::size_type({ t.extent().x, t.extent().y, t.layers() }),
+										  generate_mipmaps ? calculate_mipmap_max_level(glm::ivec2{ t.extent().x, t.extent().y }) + 1 : t.levels(),
 										  t.swizzles()) {
 		upload(t, generate_mipmaps);
 	}
 
 	// Re upload texture data. Surface must match texture's format.
-	bool upload(const gli::texture_cube &t, bool generate_mipmaps = false);
-	bool upload_face(CubeMapFace face, const gli::texture2d &t);
+	bool upload(const gli::texture_cube_array &t, bool generate_mipmaps = false);
+	bool upload_layer(int layer, const gli::texture_cube &t);
+	bool upload_face(CubeMapFace face, int layer, const gli::texture2d &t);
 
 	void upload_level(const void *data, int level = 0, int layer = 0, CubeMapFace face = CubeMapFace::CubeMapFaceNone, int data_size = 0) override {
 		assert(face != CubeMapFace::CubeMapFaceNone && "face must be specified");
@@ -39,14 +40,16 @@ public:
 
 		if (is_compressed()) {
 			assert(data_size && "size must be specified for compressed levels");
-			glCompressedTextureSubImage2D(static_cast<GLenum>(face), static_cast<GLint>(level),
-										  0, 0, std::max(1, size[0] >> level), std::max(1, size[1] >> level),
+			glCompressedTextureSubImage3D(static_cast<GLenum>(face), static_cast<GLint>(level),
+										  0, 0, layer,
+										  std::max(1, size[0] >> level), std::max(1, size[1] >> level), 1,
 										  gl_format.External,
-										  data_size, data);
+									  	  data_size, data);
 		}
 		else {
-			glTextureSubImage2D(static_cast<GLenum>(face), static_cast<GLint>(level),
-								0, 0, std::max(1, size[0] >> level), std::max(1, size[1] >> level),
+			glTextureSubImage3D(static_cast<GLenum>(face), static_cast<GLint>(level),
+								0, 0, layer,
+								std::max(1, size[0] >> level), std::max(1, size[1] >> level), 1,
 								gl_format.External, gl_format.Type,
 								data);
 		}
@@ -56,12 +59,13 @@ public:
 	void download_level(void *data,
 						std::size_t size,
 						int level,
-						CubeMapFace face) const {
+						CubeMapFace face,
+						int layer) const {
 		auto gl_format = gl_utils::translate_format(Base::format, Base::swizzle);
 		auto gl_face = static_cast<GLenum>(face);
 
 		glGetTextureSubImage(get_resource_id(), level,
-							 0, 0, gl_face - GL_TEXTURE_CUBE_MAP_POSITIVE_X,
+							 0, 0, layer * 6 + gl_face - GL_TEXTURE_CUBE_MAP_POSITIVE_X,
 							 Base::get_size().x, Base::get_size().y, 1,
 							 gl_format.External, gl_format.Type, size, data);
 	}
@@ -69,27 +73,30 @@ public:
 						std::size_t size,
 						int level,
 						CubeMapFace face,
+						int layer,
 						const gli::format &format,
 						const gli::swizzles &swizzle = swizzles_rgba,
 						bool compressed = false) const {
 		auto gl_format = gl_utils::translate_format(format, swizzle);
 		auto gl_face = static_cast<GLenum>(face);
 
-		bind();
 		if (compressed)
 			glGetCompressedTextureSubImage(get_resource_id(), level,
-										   0, 0, gl_face - GL_TEXTURE_CUBE_MAP_POSITIVE_X,
+										   0, 0, layer * 6 + gl_face - GL_TEXTURE_CUBE_MAP_POSITIVE_X,
 										   Base::get_size().x, Base::get_size().y, 1,
 										   size, data);
 		else
 			glGetTextureSubImage(get_resource_id(), level,
-								 0, 0, gl_face - GL_TEXTURE_CUBE_MAP_POSITIVE_X,
+								 0, 0, layer * 6 + gl_face - GL_TEXTURE_CUBE_MAP_POSITIVE_X,
 								 Base::get_size().x, Base::get_size().y, 1,
 								 gl_format.External, gl_format.Type, size, data);
 	}
 
-	const image<T> operator[](CubeMapFace face) const {
-		return image<T>(*this, get_image_container_size(), format, ImageAccessMode::ReadWrite, 0, static_cast<int>(face) - static_cast<int>(CubeMapFace::CubeMapFaceRight));
+	const image<T> make_image(CubeMapFace face, int layer) const {
+		return image<T>(*this,
+						get_image_container_size(),
+						format, ImageAccessMode::ReadWrite,
+						0, layer * 6 + static_cast<int>(face) - static_cast<int>(CubeMapFace::CubeMapFaceRight));
 	}
 };
 
