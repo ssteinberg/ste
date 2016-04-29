491d1d22b5248c746ff631ef453531da756a5f8a
Shlomi Steinberg <p@shlomisteinberg.com>
2016-04-29 18:02:02 +0300

    Fix IndirectDispatch class. Add indirect.glsl shader header.

---
+++ Simulation/src/ste/framework_graphics/common_shaders/indirect.glsl
@@ -0,0 +1,21 @@
+
+struct IndirectMultiDrawElementsCommand {
+	uint count;
+	uint instance_count;
+	uint first_index;
+	uint base_vertex;
+	uint base_instance;
+};
+
+struct IndirectDrawArraysCommand {
+	uint count;
+	uint prim_count;
+	uint first_index;
+	uint base_instance;
+};
+
+struct IndirectDispatchCommand {
+	uint num_groups_x;
+	uint num_groups_y;
+	uint  num_groups_z;
+};
