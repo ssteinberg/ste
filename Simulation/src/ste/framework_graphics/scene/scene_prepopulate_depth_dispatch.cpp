57f7b193938a87bcda0c55abe3ea96132033c0a0
Shlomi Steinberg <p@shlomisteinberg.com>
2016-05-01 17:16:55 +0300

    Add missing memory barriers

--- Simulation/src/ste/framework_graphics/scene/scene_prepopulate_depth_dispatch.cpp
+++ Simulation/src/ste/framework_graphics/scene/scene_prepopulate_depth_dispatch.cpp
@@ -18,5 +18,6 @@ void scene_prepopulate_depth_dispatch::set_context_state() const {
 }
 
 void scene_prepopulate_depth_dispatch::dispatch() const {
+	Core::GL::gl_current_context::get()->memory_barrier(GL_SHADER_STORAGE_BARRIER_BIT | GL_COMMAND_BARRIER_BIT);
 	scene->object_group().draw_object_group();
 }
