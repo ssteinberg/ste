d70d14f08bd4f68b10eea06b331cc9bccd5bc590
Shlomi Steinberg <p@shlomisteinberg.com>
2016-06-11 13:59:11 +0300

    Add modules' build projects

---
+++ Simulation/third_party/build/glm/test/core/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/src/git/StE/Simulation/third_party/packages/glm/test/core
+# Build directory: C:/src/git/StE/Simulation/third_party/build/glm/test/core
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ Simulation/third_party/build/zlib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/src/git/StE/Simulation/third_party/packages/zlib
+# Build directory: C:/src/git/StE/Simulation/third_party/build/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(example "example")
