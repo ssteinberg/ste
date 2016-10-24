7bf16136416e81a9f618b152f941c044bca50d6f
Shlomi Steinberg <p@shlomisteinberg.com>
2016-10-24 15:24:05 +0300

    Update GGX fittings to support v4 fits

--- Simulation/src/ste/framework_graphics/material/shaders/material_evaluate.glsl
+++ Simulation/src/ste/framework_graphics/material/shaders/material_evaluate.glsl
@@ -148,14 +148,14 @@ vec3 material_evaluate_radiance(material_layer_descriptor layer,
 										refractive_ratio);
 
 		// Evaluate total inner (downwards into material) and outer (upwards towards eye) transmission
-		float inner_transmission_ratio = ggx_transmission_ratio(microfacet_transmission_fit_lut, 
-																v, n, 
-																roughness, 
-																refractive_ratio);
-		float outer_transmission_ratio = ggx_transmission_ratio(microfacet_transmission_fit_lut, 
-																refracted_l, n, 
-																roughness, 
-																1.f / refractive_ratio);
+		float inner_transmission_ratio = ggx_transmission_ratio_v3(microfacet_transmission_fit_lut, 
+																   v, n, 
+																   roughness, 
+																   refractive_ratio);
+		float outer_transmission_ratio = ggx_transmission_ratio_v3(microfacet_transmission_fit_lut, 
+																   refracted_l, n, 
+																   roughness, 
+																   1.f / refractive_ratio);
 	
 		// Compute total and outer path lengths inside current layer
 		float dotNV = max(epsilon, dot(n,refracted_v));
