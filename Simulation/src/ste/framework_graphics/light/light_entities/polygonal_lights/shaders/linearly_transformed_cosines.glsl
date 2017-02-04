aea79ca543c44dbf7cd8dd1440db0700d8103f06
Shlomi Steinberg <p@shlomisteinberg.com>
2017-02-04 23:29:15 +0200

    LTC: Correct sphere normalization. Avoid clipping when not needed.

--- Simulation/src/ste/framework_graphics/light/light_entities/polygonal_lights/shaders/linearly_transformed_cosines.glsl
+++ Simulation/src/ste/framework_graphics/light/light_entities/polygonal_lights/shaders/linearly_transformed_cosines.glsl
@@ -33,21 +33,14 @@ float ltc_integrate_edge(vec3 v1, vec3 v2) {
 
 // Clips a quad to the horizon, returning 4 or 5 vertices.
 void ltc_clip_quad(inout vec3 L[5], out int n) {
-	// detect clipping config
 	int config = 0;
 	if (L[0].z > 0.0) config += 1;
 	if (L[1].z > 0.0) config += 2;
 	if (L[2].z > 0.0) config += 4;
 	if (L[3].z > 0.0) config += 8;
 
-	// clip
 	n = 0;
-
-	if (config == 0) {
-		// clip all
-		n = 0;
-	}
-	else if (config == 1) { // V1 clip V2 V3 V4
+	if (config == 1) { // V1 clip V2 V3 V4
 		n = 3;
 		L[1] = -L[1].z * L[0] + L[0].z * L[1];
 		L[2] = -L[3].z * L[0] + L[0].z * L[3];
@@ -66,9 +59,6 @@ void ltc_clip_quad(inout vec3 L[5], out int n) {
 		n = 3;
 		L[0] = -L[3].z * L[2] + L[2].z * L[3];
 		L[1] = -L[1].z * L[2] + L[2].z * L[1];
-	}
-	else if (config == 5) { // V1 V3 clip V2 V4) impossible
-		n = 0;
 	}
 	else if (config == 6) { // V2 V3 clip V1 V4
 		n = 4;
@@ -91,9 +81,6 @@ void ltc_clip_quad(inout vec3 L[5], out int n) {
 		L[1] = -L[1].z * L[0] + L[0].z * L[1];
 		L[2] = -L[2].z * L[3] + L[3].z * L[2];
 	}
-	else if (config == 10) { // V2 V4 clip V1 V3) impossible
-		n = 0;
-	}
 	else if (config == 11) { // V1 V2 V4 clip V3
 		n = 5;
 		L[4] = L[3];
@@ -112,13 +99,10 @@ void ltc_clip_quad(inout vec3 L[5], out int n) {
 		L[2] = -L[1].z * L[2] + L[2].z * L[1];
 		L[1] = -L[1].z * L[0] + L[0].z * L[1];
 	}
-	else if (config == 14) { // V2 V3 V4 clip V1
+	else /*if (config == 14)*/ { // V2 V3 V4 clip V1
 		n = 5;
 		L[4] = -L[0].z * L[3] + L[3].z * L[0];
 		L[0] = -L[0].z * L[1] + L[1].z * L[0];
-	}
-	else if (config == 15) { // V1 V2 V3 V4
-		n = 4;
 	}
 	
 	if (n == 3)
@@ -129,19 +113,13 @@ void ltc_clip_quad(inout vec3 L[5], out int n) {
 
 // Clips a triangle to the horizon, returning 3 or 4 vertices.
 void ltc_clip_triangle(inout vec3 L[4], out int n) {
-	// detect clipping config
 	int config = 0;
 	if (L[0].z > 0.0) config += 1;
 	if (L[1].z > 0.0) config += 2;
 	if (L[2].z > 0.0) config += 4;
 
-	// clip
 	n = 0;
-
-	if (config == 0) {
-		// clip all
-	}
-	else if (config == 1) { // V1 clip V2 V3
+	if (config == 1) { // V1 clip V2 V3
 		n = 3;
 		L[1] = -L[1].z * L[0] + L[0].z * L[1];
 		L[2] = -L[2].z * L[0] + L[0].z * L[2];
@@ -167,13 +145,10 @@ void ltc_clip_triangle(inout vec3 L[4], out int n) {
 		L[2] = -L[2].z * L[1] + L[1].z * L[2];
 		L[1] = -L[1].z * L[0] + L[0].z * L[1];
 	}
-	else if (config == 6) { // V2 V3 clip V1
+	else /*if (config == 6)*/ { // V2 V3 clip V1
 		n = 4;
 		L[3] = -L[0].z * L[2] + L[2].z * L[0];
 		L[0] = -L[0].z * L[1] + L[1].z * L[0];
-	}
-	else if (config == 7) { // V1 V2 V3
-		n = 3;
 	}
 	
 	if (n == 3)
@@ -195,17 +170,24 @@ vec3 ltc_evaluate_quad(vec3 N, vec3 V, vec3 P, mat3 Minv, vec3 light_pos, uint o
 	L[1] = Minv * (light_pos + ltc_points[offset + 1].xyz - P);
 	L[2] = Minv * (light_pos + ltc_points[offset + 2].xyz - P);
 	L[3] = Minv * (light_pos + ltc_points[offset + 3].xyz - P);
+	L[4] = L[0];
 
 	vec3 textureLight = vec3(1, 1, 1);
 #if LTC_TEXTURED
 	//textureLight = FetchDiffuseFilteredTexture(texFilteredMap, L[0], L[1], L[2], L[3]);
 #endif
 
+	vec4 zs = vec4(L[0].z, L[1].z, L[2].z, L[3].z);
+	bool all_below_horizon = all(lessThanEqual(zs, vec4(0)));
+	bool all_above_horizon = all(greaterThanEqual(zs, vec4(0)));
+
 	int n;
-	ltc_clip_quad(L, n);
-	
-	if (n == 0)
+	if (all_below_horizon)
 		return vec3(.0f);
+	else if (all_above_horizon)
+		n = 4;
+	else
+		ltc_clip_quad(L, n);
 
 	// project onto sphere
 	L[0] = normalize(L[0]);
@@ -257,12 +239,18 @@ vec3 ltc_evaluate_polygon(vec3 N, vec3 V, vec3 P, mat3 Minv, vec3 light_pos, uin
 	#if LTC_TEXTURED
 		//textureLight = FetchDiffuseFilteredTexture(texFilteredMap, L[0], L[1], L[2], L[3]);
 	#endif
-
-		int n=3;
-		ltc_clip_triangle(L, n);
 	
-		if (n == 0)
-			return vec3(0, 0, 0);
+		vec3 zs = vec3(L[0].z, L[1].z, L[2].z);
+		bool all_below_horizon = all(lessThanEqual(zs, vec3(.0f)));
+		bool all_above_horizon = all(greaterThanEqual(zs, vec3(.0f)));
+
+		int n;
+		if (all_below_horizon)
+			continue;
+		else if (all_above_horizon)
+			n = 3;
+		else
+			ltc_clip_triangle(L, n);
 
 		// project onto sphere
 		L[0] = normalize(L[0]);
@@ -344,5 +332,5 @@ vec3 ltc_evaluate_sphere(vec3 N, vec3 V, vec3 P, mat3 Minv, vec3 light_pos, floa
 	
 	sum += ltc_integrate_edge(p0, start_point);
 
-	return abs(sum).xxx;
+	return abs(sum).xxx / two_pi;
 }
