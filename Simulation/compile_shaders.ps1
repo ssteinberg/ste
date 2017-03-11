c13203b630dac34fc35c675efd0d15db28bac9c4
Shlomi Steinberg <p@shlomisteinberg.com>
2017-03-11 02:21:59 +0200

    Modify spir-v shader compilation script

--- Simulation/compile_shaders.ps1
+++ Simulation/compile_shaders.ps1
@@ -1,6 +1,14 @@
 param([string]$d=".")
 
-$files = Get-ChildItem -Path $d\src\* -Include *.vert, *.geom, *.frag, *.comp -Recurse 
+$timestamp = "compile_shader_timestamp.temp"
+$date = 0
+if (Test-Path $timestamp) {
+	$date = (Get-Item $timestamp).LastWriteTime
+}
+
+$files = Get-ChildItem -Path $d\src\* -Include *.vert, *.geom, *.frag, *.comp -Recurse | Where-Object { $_.LastWriteTime -gt $date }
 foreach ($f in $files) {
 	& "$d\Tools\ste_spirv_compiler\ste_spirv_compiler.exe" $f.FullName
 }
+
+Out-File $timestamp
--- compile_shaders.ps1
+++
@@ -1,2 +0,0 @@
-
-Get-ChildItem -Path ./Simulation/src/* -Include *.vert, *.geom, *.frag, *.comp -Recurse | % { .\Simulation\Tools\ste_spirv_compiler\ste_spirv_compiler.exe $_.FullName }
