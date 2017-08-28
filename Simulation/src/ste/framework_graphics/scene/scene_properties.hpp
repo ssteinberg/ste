d339819f602aa56e22c1b4f0a49f99cd47d38522
Shlomi Steinberg <p@shlomisteinberg.com>
2017-08-28 11:34:53 +0300

    Separate material storage and material textures storage

--- Simulation/src/ste/framework_graphics/scene/scene_properties.hpp
+++ Simulation/src/ste/framework_graphics/scene/scene_properties.hpp
@@ -5,6 +5,7 @@
 
 #include <material_storage.hpp>
 #include <material_layer_storage.hpp>
+#include <material_textures_storage.hpp>
 #include <light_storage.hpp>
 
 #include <command_recorder.hpp>
@@ -16,12 +17,16 @@ class scene_properties {
 private:
 	material_storage materials;
 	material_layer_storage material_layers;
+
+	material_textures_storage material_textures;
+
 	light_storage lights;
 
 public:
 	scene_properties(const ste_context &ctx) 
 		: materials(ctx),
-		material_layers(ctx, materials.get_material_texture_storage()),
+		material_layers(ctx,
+						material_textures),
 		lights(ctx)
 	{}
 
@@ -29,6 +34,10 @@ public:
 	auto& materials_storage() const { return materials; }
 	auto& material_layers_storage() { return material_layers; }
 	auto& material_layers_storage() const { return material_layers; }
+
+	auto& material_textures_storage() { return material_textures; }
+	auto& material_textures_storage() const { return material_textures; }
+
 	auto& lights_storage() { return lights; }
 	auto& lights_storage() const { return lights; }
 
